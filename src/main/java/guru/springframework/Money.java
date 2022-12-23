63f4a91a33ff1e81c5a78a5a96c1ee55fae42a2d
Norvald Espedal <norvald.espedal@outlook.com>
2022-12-23 22:01:17 +0100

    Lecture 22

---
+++ src/main/java/guru/springframework/Money.java
@@ -0,0 +1,19 @@
+package guru.springframework;
+
+public class Money {
+    protected int amount;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Money money = (Money) o;
+
+        return amount == money.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return amount;
+    }
+}
